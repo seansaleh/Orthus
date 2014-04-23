8d03bb3030e4c2fe72112ce8fccd8c3f9da1b5a0
Sean Saleh <github@seansaleh.com>
2014-04-23 12:12:43 -0700

    Config now localhost default

--- config/config.js
+++ config/config.js
@@ -1,7 +1,4 @@
 ﻿module.exports = {
-    //port:1337,
     port: process.env.port || 80,
-    //proxiedResource: "http://localhost:8080"
-    proxiedResource: "http://ghs.l.google.com:80"
-
+    proxiedResource: "http://localhost:8080"
 };
