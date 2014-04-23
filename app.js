2da3361c71a974484d0a225a011918a30033d89c
Sean Saleh <github@seansaleh.com>
2014-04-23 12:03:30 -0700

    No Authentication proxying working

---
+++ app.js
@@ -0,0 +1,27 @@
+﻿var http = require('http');
+var express = require('express');
+//var passport = require('passport');
+var httpProxy = require('http-proxy');
+
+//Import own JS files
+var config = require('./config/config');
+
+//App Setup
+var app = express();
+var proxy = httpProxy.createProxyServer({
+    target: config.proxiedResource
+});
+
+
+//Routes
+app.use(function (req, res, next) {
+    proxy.web(req, res);
+});
+
+app.on('upgrade', function (req, socket, head) {
+    proxy.ws(req, socket, head);
+});
+
+//Finally start server
+app.listen(config.port);
+console.log("listening on port: " + config.port);
---
+++ config/config.js
@@ -0,0 +1,7 @@
+﻿module.exports = {
+    //port:1337,
+    port: process.env.port || 80,
+    //proxiedResource: "http://localhost:8080"
+    proxiedResource: "http://ghs.l.google.com:80"
+
+};
