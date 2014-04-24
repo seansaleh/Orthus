3d01eae6c33dd4e1413c403e901ca7d26d29675c
Sean Saleh <github@seansaleh.com>
2014-04-23 20:42:13 -0700

    OpenID is a deadend. Too buggy for not google

--- app.js
+++ app.js
@@ -1,34 +1,78 @@
 ﻿var http = require('http');
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var passport = require('passport');
 var OpenIDStrategy = require('passport-openid').Strategy;
 var httpProxy = require('http-proxy');
 
-//Import own JS files
+/* Import own JS files */
 var config = require('./config/config');
 
-//App Setup
+/* App Setup */
 var app = express();
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(bodyParser.urlencoded());
+app.use(cookieParser())
+app.use(session({ key: 'orthus', secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 var proxy = httpProxy.createProxyServer({
     target: config.proxiedResource
 });
 
 passport.use(new OpenIDStrategy({
     returnURL: config.serverAddress + '/auth/openid/callback',
-    realm: config.serverAddress,
     profile: true,
     },
     function (identifier, profile, done) {
         return done(null, { identifier: identifier, profile: profile });
     }
 ));
+passport.serializeUser(function (user, done) {
+    done(null, { identifier: user.identifier, profile: user.profile });
+});
 
+passport.deserializeUser(function (user, done) {
+    done(null, { identifier: user.identifier, profile: user.profile });
+});
 
-//Routes
-app.get('/auth/openid', passport.authenticate('openid'));
+/** Routes **/
+app.post('/auth/openid', passport.authenticate('openid'),
+  function (req, res) {
+      res.redirect('/');
+  });
 app.get('/auth/openid/callback', passport.authenticate('openid', { failureRedirect: '/login' }), function (req, res) {
     res.redirect(req.session.returnTo || '/');
+    res.redirect('/');
 });
+
+app.use('/login', express.static(__dirname + '/public/login'));
+app.get('/login', function (req, res, next) {
+    res.sendfile(__dirname + '/public/login/login.html');
+});
+
+app.get('/logout', function (req, res, next) {
+    req.logout();
+    res.redirect('/login');
+});
+
+app.get('/signup', function (req, res, next) {
+    res.redirect('/login');
+});
+
+
+//Anything after this needs to be authenticated
+app.use(function (req, res, next) {
+    if (req.isAuthenticated()) return next();
+    req.session.returnTo = req.path;
+    res.redirect('/login');
+});
+
 app.use(function (req, res, next) {
     proxy.web(req, res);
 });
@@ -40,3 +84,7 @@ app.on('upgrade', function (req, socket, head) {
 //Finally start server
 app.listen(config.port);
 console.log("listening on port: " + config.port);
+
+process.on('uncaughtException', function (exception) {
+    debugger;
+});
--- config/config.js
+++ config/config.js
@@ -1,5 +1,8 @@
 ﻿module.exports = {
     port: process.env.port || 1337,
-    serverAddress: 'http://localhost:1337',
-    proxiedResource: "http://localhost:8080"
+    //serverAddress: 'http://localhost:1337',
+    //proxiedResource: "http://localhost:8080"
+    serverAddress: 'http://www.seansaleh.com:1337',
+    proxiedResource: "http://ghs.l.google.com:80"
+
 };
