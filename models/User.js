e6f1a372498bb77052f287c6e3068fd952fa9f5a
Sean Saleh <github@seansaleh.com>
2014-04-24 18:47:50 -0700

    Using own version of node-persist to simplify key encoding

--- models/User.js
+++ models/User.js
@@ -1,9 +1,5 @@
 ﻿var storage = require('node-persist');
-storage.initSync();
-
-function encodeKey(string) { //needed since identifier name can't be written to disk
-    return new Buffer(string).toString('base64');
-}
+storage.initSync({encodeFilename: true});
 
 var User = function (identifier, profile) {
     this.identifier = identifier;
@@ -11,13 +7,13 @@ var User = function (identifier, profile) {
 };
 
 User.findByIdentifier = function (identifier, callback) {
-    return callback(null, storage.getItem(encodeKey(identifier)));
+    return callback(null, storage.getItem(identifier));
 };
 
 User.create = function (identifier, profile) {
     if (!identifier || !profile) return;
     var user = new User(identifier, profile);
     user.isAuthorized = true;
-    storage.setItem(encodeKey(identifier), user);
+    storage.setItem(identifier, user);
 };
 module.exports = User;
