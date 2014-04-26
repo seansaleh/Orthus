f1d6fc0f23d7c0264da7da22e347855259c70c7b
Sean Saleh <seansaleh@gmail.com>
2014-04-26 14:11:24 -0700

    Can now take config parameters in process.env

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 ﻿module.exports = {
     port: process.env.port || 1337,
-    openIDReturnURL: 'http://localhost:1337/orthus/auth/openid/callback',
+    openIDReturnURL: process.env.orthus-openIDURL || 'http://localhost:1337/orthus/auth/openid/callback',
     //proxiedResource: "http://localhost:8080",
     //These are temporariry conveiences for fast development. AKA am too lazy to put it elsewhere :)
-    proxiedResource: "http://ghs.l.google.com:80",
+    proxiedResource: process.env.orthus-proxied ||"http://ghs.l.google.com:80",
     baseURL: '/orthus/',
 };
