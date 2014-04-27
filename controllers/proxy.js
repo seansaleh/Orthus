a8e015ad51a78c441d71d33b2fa4b04d475aede2
Sean Saleh <github@seansaleh.com>
2014-04-27 12:37:48 -0700

    Added error handling to proxy

--- controllers/proxy.js
+++ controllers/proxy.js
@@ -6,9 +6,18 @@ var proxy = httpProxy.createProxyServer({
 });
 
 exports.proxy = function (req, res, next) {
-    proxy.web(req, res);
+    proxy.web(req, res, next);
 };
 
 exports.proxyWebSocket = function (req, socket, head) {
     proxy.ws(req, socket, head);
 };
+
+proxy.on('error', function (err, req, res) {
+    res.writeHead(500, {
+        'Content-Type': 'text/plain'
+    });
+    console.error("error in proxy");
+    console.dir(err);
+    res.end('Something went wrong, sorry!');
+});
