bce8e18e5a70070bb8f1ad891257bc9797d27a3e
Sean Saleh <github@seansaleh.com>
2014-04-27 20:08:38 -0700

    started writing some tests

--- config/config.js
+++ config/config.js
@@ -1,8 +1,6 @@
 ﻿module.exports = {
     port: process.env.port || 1337,
     openIDReturnURL: process.env.orthus_openIDURL || 'http://localhost:1337/orthus/auth/openid/callback',
-    //proxiedResource: "http://localhost:8080",
-    //These are temporariry conveiences for fast development. AKA am too lazy to put it elsewhere :)
-    proxiedResource: process.env.orthus_proxied ||"http://ghs.l.google.com:80",
+    proxiedResource: process.env.orthus_proxied ||"http://localhost:8080",
     baseURL: '/orthus/',
 };
---
+++ test/stream-latency.js
@@ -0,0 +1,50 @@
+﻿var http = require('http');
+var config = require('../config/config');
+
+describe('Proxy Tests', function () {
+    var proxiedServer;
+    var proxyController;
+    var proxyServer;
+    before(function () {
+        proxyController = require('../controllers/proxy');
+
+        proxiedServer = http.createServer(function (req, res) {
+            console.log("==Proxied server responding==");
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2));
+            res.end();
+        }).listen(8080);
+
+        proxyServer = http.createServer(function (req, res) {
+            console.log("==Proxy Server forwarding to proxyController==");
+            console.log(config.proxiedResource);
+            proxyController.proxy(req, res);
+        }).listen(1337);
+
+        console.log("before all done");
+    });
+
+    beforeEach(function () {
+
+    });
+
+    it("test", function (done) {
+        var request = http.request({ port:1337}, function (res) {
+            console.log('STATUS: ' + res.statusCode);
+            console.log('HEADERS: ' + JSON.stringify(res.headers));
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                console.log('BODY: ' + chunk);
+            });
+            res.on('end', done);
+        })
+        .on("error", function (e) {
+            console.error(e);
+            //done(e);
+        });
+        request.write("test")
+        request.end();
+    });
+
+
+});
