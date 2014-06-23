abfe7083fdf4904d2666421967ca2b2c9d6e7267
Sean Saleh <seansaleh@gmail.com>
2014-06-23 11:54:30 -0700

    Option to specify persona URL from process.env

--- config/config.js
+++ config/config.js
@@ -1,8 +1,14 @@
-﻿module.exports = {
+﻿var baseURL = '/orthus/';
+var openIDReturnURLFromOrthusURL;
+if (process.env.orthus_url) {
+	openIDReturnURLFromOrthusURL = process.env.orthus_url + baseURL + 'auth/openid/callback'
+}
+//TODO: clean up this code more. Maybe even use nconf
+module.exports = {
     port: process.env.port || 1337,
-    openIDReturnURL: process.env.orthus_openIDURL || 'http://localhost:1337/orthus/auth/openid/callback',
+    openIDReturnURL: openIDReturnURLFromOrthusURL || process.env.orthus_openIDURL || 'http://localhost:1337/orthus/auth/openid/callback',
     proxiedResource: process.env.orthus_proxied ||"http://localhost:8080",
-    baseURL: '/orthus/',
-    personaAudience: "http://localhost:1337",
+    baseURL: baseURL,
+    personaAudience: process.env.orthus_url || "http://localhost:1337",
     addAuthHeader: (process.env.orthus_addAuthHeader=="true") || false,
 };
